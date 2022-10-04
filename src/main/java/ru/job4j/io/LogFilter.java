0687e1132a9e37eda83b57af7cabee800afb721f
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-10-05 00:39:29 +0300

    0.3. BufferedReader. [#252489]

---
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LogFilter {
+    public List<String> filter(String file) {
+        try (BufferedReader in = new BufferedReader(new FileReader(file))) {
+            return in.lines()
+                    .filter(l -> l.split("\\s")[l.split("\\s").length - 2].equals("404"))
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        LogFilter logFilter = new LogFilter();
+        List<String> log = logFilter.filter("log.txt");
+        log.stream().forEach(System.out::println);
+    }
+}
