1dfd939e339a7685e6922f83e69ecbd2caf00fc6
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-10-09 00:47:31 +0300

    2. Анализ доступности сервера. [#859 #349513]

---
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,46 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.util.StringJoiner;
+
+public class Analizy {
+    public void unavailable(String source, String target) {
+        try (BufferedReader log = new BufferedReader(new FileReader(source))) {
+            String line;
+            String status;
+            String previousStatus = "";
+            StringJoiner resultLine = new StringJoiner(";");
+            try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+                while ((line = log.readLine()) != null) {
+                    status = line.substring(0, 3);
+                    if (statusIsUnavailable(status) && !statusIsUnavailable(previousStatus)) {
+                        resultLine.add(line.substring(4));
+                    }
+                    if (!statusIsUnavailable(status) && statusIsUnavailable(previousStatus)) {
+                        resultLine.add(line.substring(4));
+                        out.println(resultLine);
+                        resultLine = new StringJoiner(";");
+                    }
+                    previousStatus = status;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean statusIsUnavailable(String status) {
+        return "400".equals(status) || "500".equals(status);
+    }
+
+    public static void main(String[] args) {
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("15:01:30;15:02:32");
+            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
