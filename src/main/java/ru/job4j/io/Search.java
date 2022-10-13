f4a1a355f7267fbf8a20b8f2d684a36c05061273
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-10-14 00:57:54 +0300

    5. Валидация параметров запуска. [#246865]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -10,8 +11,9 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        search(start, p -> p.toFile().getName().endsWith(".js")).forEach(System.out::println);
+        validateArgs(args);
+        Path start = Paths.get(args[0]);
+        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) {
@@ -22,6 +24,23 @@ public class Search {
             throw new RuntimeException(e);
         }
         return searcher.getPaths();
+    }
+
+    private static void validateArgs(String[] args) {
+        if (args.length != 2) {
+            throw new IllegalArgumentException("Root folder or filename extension is null.");
+        }
+        File file = new File(args[0]);
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Root folder is not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Root folder is not directory %s", file.getAbsoluteFile()));
+        }
+        if (!args[1].startsWith(".")) {
+            throw new IllegalArgumentException("Invalid filename extension");
+        }
+
     }
 
     private static class SearchFiles extends SimpleFileVisitor<Path> {
