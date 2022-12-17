6560a3f79987747e22302ceadb7bc1f1b7fa29c9
Dmitriy Afonichkin <dafonichkin@gmail.com>
2022-12-17 23:46:56 +0300

    0.2. PreparedStatement [#379307 #371365]

--- src/main/java/ru/job4j/spammer/ImportDB.java
+++ src/main/java/ru/job4j/spammer/ImportDB.java
@@ -16,7 +16,7 @@ public class ImportDB {
         this.dump = dump;
     }
 
-    public List<User> load() throws IOException, IllegalArgumentException {
+    public List<User> load() throws IOException {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
             rd.lines().forEach(s -> users.add(convertStringToUser(s)));
