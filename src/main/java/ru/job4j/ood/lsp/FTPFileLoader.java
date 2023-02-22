b6bde5d7c1d0911341fe8b6ea0ef1d3ea23405ba
Dmitriy Afonichkin <dafonichkin@gmail.com>
2023-02-23 01:04:21 +0300

    0. Принцип подстановки Лисков. [#4915]

--- src/main/java/ru/job4j/ood/lsp/FTPFileLoader.java
+++ src/main/java/ru/job4j/ood/lsp/FTPFileLoader.java
@@ -1,6 +1,15 @@
 package ru.job4j.ood.lsp;
 
 public class FTPFileLoader extends FileLoader {
+    private String driveLetter;
+    public FTPFileLoader(String driveLetter) {
+        super(driveLetter);
+    }
+
+    public void setDriveLetter(String driveLetter) {
+        this.driveLetter = driveLetter;
+    }
+
     @Override
     public void load(int fileSize) {
         if (fileSize > 5000) {
--- src/main/java/ru/job4j/ood/lsp/FileLoader.java
+++ src/main/java/ru/job4j/ood/lsp/FileLoader.java
@@ -1,9 +1,27 @@
 package ru.job4j.ood.lsp;
 
 public class FileLoader {
+    private String driveLetter;
+
+    public FileLoader(String driveLetter) {
+        if (driveLetter.length() != 1) {
+            throw new IllegalArgumentException("Drive letter not correct");
+        }
+        this.driveLetter = driveLetter;
+    }
+
+    public void setDriveLetter(String driveLetter) {
+        if (driveLetter.length() != 1) {
+            throw new IllegalArgumentException("Drive letter not correct");
+        }
+        this.driveLetter = driveLetter;
+    }
+
     public void load(int fileSize) {
         if (fileSize > 1000) {
             throw new IllegalArgumentException("File is too big");
         }
     }
+
+
 }
