9a0543acbb507cc6764fb431587862aa37cc40f0
Dmitriy Afonichkin <dafonichkin@gmail.com>
2023-03-10 23:57:25 +0300

    0. Принцип инверсии зависимостей [#4917]

---
+++ src/main/java/ru/job4j/ood/dip/ITDepartment.java
@@ -0,0 +1,9 @@
+package ru.job4j.ood.dip;
+
+public class ITDepartment implements Department {
+
+    @Override
+    public ITDepartment getITDepartment() {
+        return this;
+    }
+}
---
+++ src/main/java/ru/job4j/ood/dip/SalaryService.java
@@ -0,0 +1,14 @@
+package ru.job4j.ood.dip;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SalaryService {
+
+    List<Employee> employeeList = new ArrayList<>();
+
+    void logResult(String rsl) {
+        System.out.println(rsl);
+    }
+
+}
